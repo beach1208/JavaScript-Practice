3cb5764c1fb3715671495707b186c08b93e0a42b
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-10-30 21:21:31 -0700

    new question done

---
+++ FreeCodeCamp/ChunkyMonkey.js
@@ -0,0 +1,10 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  let newArray = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArray.push(arr.slice(i, i + size));
+  }
+  return newArray;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
