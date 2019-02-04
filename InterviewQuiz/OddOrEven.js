277cf5d30428a64817f76e009cb304b7e43faf2e
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-02-04 14:01:23 -0800

    reduce challenge done

--- InterviewQuiz/OddOrEven.js
+++ InterviewQuiz/OddOrEven.js
@@ -3,3 +3,29 @@ function oddOrEven(array) {
 }
 
 console.log(oddOrEven([2, 5, 34, 6]));
+
+//reduce challenge
+let data = [
+  {
+    country: 'China',
+    pop: 1409517397,
+  },
+  {
+    country: 'India',
+    pop: 1339180127,
+  },
+  {
+    country: 'USA',
+    pop: 324459463,
+  },
+  {
+    country: 'Indonesia',
+    pop: 263991379,
+  }
+]
+
+
+let sum = data.reduce((acc,val) => {
+    return val.country == 'China' ? acc : acc + val.pop;
+},0);
+console.log(sum);
