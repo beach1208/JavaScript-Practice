44367a3533a4819036f905064dab606b61d27064
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-03-16 11:50:08 -0700

    DOM practice done

--- JavaScript-DOM-Tutorial-lesson-10/app.js
+++ JavaScript-DOM-Tutorial-lesson-10/app.js
@@ -1,20 +1,19 @@
 //Get by classname need to be changed to arrays
 var titles = document.getElementsByClassName("title");
 
-// console.log(Array.isArray(titles));
+console.log(Array.isArray(titles));
 
 //changing html collections to array
-// console.log(Array.isArray(Array.from(titles)));
+console.log(Array.isArray(Array.from(titles)));
 
-// Array.from(titles).forEach(item => {
-//   console.log(item);
-// });
+Array.from(titles).forEach(item => {
+  console.log(item);
+});
 
-// const wrap = document.querySelector("#book-list li:nth-child(2) .name");
-// console.log(wrap);
+const wrap = document.querySelector("#book-list li:nth-child(2) .name");
+console.log(wrap);
 
-//querrySelectorAll = return Nodelist
-//no need to change to array for loop
+
 
 //get first element of li
 var books = document.querySelector("#book-list li .name");
@@ -22,34 +21,40 @@ var books = document.querySelector("#book-list li .name");
 books = document.querySelectorAll("#book-list li .name");
 console.log(books);
 
-//all elements change to array and log it console
-Array.from(books).forEach(book => {
-  book.textContent += "(book title)";
-});
+// //all elements change to array and log it console
+// Array.from(books).forEach(book => {
+//   book.textContent += "(book title)";
+// });
 
 const bookList = document.querySelector("#book-list");
 // bookList.innerHTML = "<h2>Books and more books...</h2>";
 bookList.innerHTML += "<p>This is how you add HTML</p>";
 
-//what is node?
+// const bookList = document.querySelectorAll(".name");
+// console.log(bookList);
+// Array.from(bookList).forEach(mybook => {
+//   mybook.innerHTML += "<p>This is how you add HTML</p>"
+// })
+
+// //what is node?
 const banner = document.querySelector("#page-banner");
 console.log("#page-banner node type is", banner.nodeType);
 console.log("#page-banner node name is", banner.nodeName);
 console.log("#page-banner node has childNode", banner.hasChildNodes()); //return true or false
 
-//clone node
+// //clone node
 const clonedBanner = banner.cloneNode(true);
 //returns all elements inside page-banner elements
 console.log(clonedBanner);
 
-//parentnode
+// //parentnode
 const bookList2 = document.querySelector("#book-list");
 console.log("the parent node is:", bookList2.parentNode);
-//get parent of parent
+// //get parent of parent
 console.log("the parent element is:", bookList2.parentElement.parentElement);
 console.log(bookList2.children);
 
-//get sibling node
+// //get sibling node
 const bookList3 = document.querySelector("#book-list");
 console.log("book-list next sibling is", bookList.nextSibling);
 console.log("book-list next element sibling is", bookList.nextElementSibling);
@@ -62,3 +67,86 @@ console.log(
 
 bookList.previousElementSibling.querySelector("p").innerHTML +=
   "<br/>Too cool for everyone";
+
+// var btns = document.querySelectorAll("#book-list .delete");
+
+// Array.from(btns).forEach(btn => {
+//   btn.addEventListener('click', e =>{
+//     //get element you clicked
+//     const li = e.target.parentElement;
+
+//     //remove child li from ul
+//     li.parentNode.removeChild(li);
+//   });
+// });
+
+// const link = document.querySelector('#page-banner a');
+// link.addEventListener('click',e => {
+//   e.preventDefault();
+//   console.log('navigation to', e.target.textContent);
+// })
+
+// remove elements once delete btn clicked
+const list = document.querySelector('#book-list ul');
+
+list.addEventListener('click', e => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    list.removeChild(li);
+  }
+})
+
+//add book-list
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit',e => {
+  e.preventDefault();
+  //get text of user typed
+  const value = addForm.querySelector('input[type="text"]').value;
+  
+  //create Elements
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  //add content
+  deleteBtn.textContent = 'delete';
+  bookName.textContent = value;
+
+  //add classname
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  //Append those elements to dom
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+})
+
+
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', e => {
+
+  if(hideBox.checked){
+    list.style.display = 'none';
+  }else {
+    list.style.display = 'block';
+  }
+})
+
+//filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', e => {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(book => {
+    //grabbing span
+    const title = book.firstElementChild.textContent;
+    //check if title is same as term
+    if(title.toLowerCase().indexOf(term) != -1){
+      book.style.display = 'block';
+    }else {
+      book.style.display = 'none';
+    }
+  })
+})
